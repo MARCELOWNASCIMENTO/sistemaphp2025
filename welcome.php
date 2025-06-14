6ca9fce559b20934475d3975699ab71ee2026253
marcelownascimento <contato@marcelownascimento.com.br>
2025-06-14 16:54:13 -0300

    primeiro commit: adicionando sistema2

---
+++ welcome.php
@@ -0,0 +1,96 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: index.php");
+    exit;
+}
+?>
+
+<style type="text/css">
+	/* Dropdown Button */
+.dropbtn {
+  background-color: #04AA6D;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+}
+
+/* The container <div> - needed to position the dropdown content */
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+/* Change color of dropdown links on hover */
+.dropdown-content a:hover {background-color: #ddd;}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {display: block;}
+
+/* Change the background color of the dropdown button when the dropdown content is shown */
+.dropdown:hover .dropbtn {background-color: #3e8e41;}
+</style>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+<hr>
+	 <h1 class="my-5">Ola, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Bem vindo!.</h1>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+    </p>
+<hr>
+
+
+<div class="dropdown">
+  <button class="dropbtn">Equipamentos</button>
+  <div class="dropdown-content">
+    <a href="#">Inclusao</a>
+    <a href="#">Consulta</a>
+  </div>
+
+</div>
+<div class="dropdown">
+  <button class="dropbtn">Servicos</button>
+  <div class="dropdown-content">
+    <a href="#">Inclusao</a>
+    <a href="#">Consulta</a>
+  </div>
+
+</div>
+
+
+
+</body>
+</html>
